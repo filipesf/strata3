b8d46355cbc9ff1866513adcc5cc89d1f8b09990
Filipe Fernandes <me@filipesf.com>
2018-09-17 15:22:10 +0100

    [refs #1] Retrieving raw data from NewsAPI

--- source/App.js
+++ source/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './assets/styles/main.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+
+    this.apiUrl =
+      'https://newsapi.org/v2/everything?q=brazil&language=en&apiKey=3f21c88dbb144b2d92a6065158741d10';
+  }
+
+  componentWillMount() {
+    axios.get(this.apiUrl).then(res => {
+      this.setState({ data: res.data.articles });
+      console.log(this.state.data);
+    });
+  }
+
   render() {
     return (
       <div />
