77879b1bef5be60fd497d72d25186af95db28d2e
Filipe Fernandes <me@filipesf.com>
2018-09-17 22:38:24 +0100

    [refs #2 #4] Finished card's design 👇🏾👇🏾👇🏾
    - New "hero" card design;
    - Adjusted card tag colour and positioning;
    - Added a gradient background mask to improve readability;
    - Adjusted elevation transitions;
    - Removed `.c-card__image`;
    - Adjusted `$elevations` colour;

--- source/components/Card.js
+++ source/components/Card.js
@@ -5,13 +5,8 @@ const Card = props => {
   const c = props.content;
 
   return (
-    <a className="c-card" href="#">
+    <a className="c-card" href="#" style={{backgroundImage: `url(${c.urlToImage})`}}>
       <strong className="c-card__tag">{c.source.name}</strong>
-
-      <figure
-        className="c-card__image"
-        style={{backgroundImage: `url(${c.urlToImage})`}}
-      />
 
       <article className="c-card__content">
         <h6 className="c-card__title">{c.title}</h6>
