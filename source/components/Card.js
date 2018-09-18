1f4d83630e07174b92724f69b6b06a1593bb5c15
Filipe Fernandes <me@filipesf.com>
2018-09-18 12:36:47 +0100

    [refs #4 #5] Added modal support; minor code styling

--- source/components/Card.js
+++ source/components/Card.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Modal from './Modal';
 
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showModal: false };
 
-const Card = props => {
-  const c = props.content;
+    this.handleModal = this.handleModal.bind(this);
+  }
 
-  return (
-    <a className="c-card" href="#" style={{backgroundImage: `url(${c.urlToImage})`}}>
-      <strong className="c-card__tag">{c.source.name}</strong>
+  handleModal = () => {
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  };
 
-      <article className="c-card__content">
-        <h6 className="c-card__title">{c.title}</h6>
-        <p className="c-card__description">{c.description}</p>
-      </article>
-    </a>
-  );
-};
+  render() {
+    const c = this.props.content;
+
+    return (
+      <div>
+        <Modal
+          isVisible={this.state.showModal}
+          handleClose={this.handleModal}
+          content={this.props.content}
+        />
+
+        <div
+          className="c-card"
+          style={{ backgroundImage: `url(${c.urlToImage})` }}
+          onClick={this.handleModal}>
+
+          <strong className="c-card__tag">{c.source.name}</strong>
+          <article className="c-card__content">
+            <h6 className="c-card__title">{c.title}</h6>
+            <p className="c-card__description">{c.description}</p>
+          </article>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Card;
